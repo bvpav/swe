17da027ff5659bcb8f4dbf23620c57e34b92e367
bvpav <78971592+bvpav@users.noreply.github.com>
2025-04-01 13:23:16 +0300

    forgot to config rector paths

--- rector.php
+++ rector.php
@@ -6,6 +6,11 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        
+        __DIR__ . '/src',
+    ]);
+
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
     ]);
